d3a0415e4a8e403c7b0a278a4b21937db5f248ec
sBobshit <44058159+sBobshit@users.noreply.github.com>
2020-07-05 13:04:02 +0300

    Create downloadFromOxford.js

---
+++ downloadFromOxford.js
@@ -0,0 +1,11 @@
+var curent = 1;
+var pages = [];
+$("#prev-page-move-icon").click(function () {
+console.log("current", current);
+var pg1 = $(`book-image-${current}`).attr("src");
+var pg2 = $(`book-image-${current + 1}`).attr("src");
+pages.push(pg1, pg2);
+console.log(pages);
+current++;
+current++;
+})
