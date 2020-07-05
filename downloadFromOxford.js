ced452ea72c48398024acb0e98969108e2c1bea5
sBobshit <44058159+sBobshit@users.noreply.github.com>
2020-07-05 13:11:17 +0300

    Update downloadFromOxford.js

--- downloadFromOxford.js
+++ downloadFromOxford.js
@@ -1,6 +1,6 @@
 var curent = 1;
 var pages = [];
-$("#prev-page-move-icon").click(function () {
+$(".prev-page-move-icon").click(function () {
 console.log("current", current);
 var pg1 = $(`book-image-${current}`).attr("src");
 var pg2 = $(`book-image-${current + 1}`).attr("src");
